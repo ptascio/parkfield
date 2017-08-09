5a786abd2abce73c837169f71ac6fc3b46160af4
Peter Tascio <peter.tascio@gmail.com>
2017-08-09 15:26:37 -0400

    Work on node distribution

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var manualItems;
 var instaGramItems;
 var twitterItems;
+var rowDiv;
+var colDiv;
+var imgTag;
+var pTag;
+
 
 $(document).ready(function() {
     loadData();
-
 });
 
 
@@ -21,6 +25,7 @@ function loadData(){
     filterResults("Manual", "manualItems");
     filterResults("Instagram", "instaGramItems");
     filterResults("Twitter", "twitterItems");
+
   },
   error: function (error) {
     $("#error").html(error.responseJSON["error"]);
@@ -30,10 +35,71 @@ function loadData(){
 
 
 
-function filterResults(type, container){
+function filterResults(type){
   var itemsArray = content["items "];
-  container = itemsArray.filter(function(item, i){
+  var container = itemsArray.filter(function(item, i){
     return item["service_name"] === type;
   });
-console.log(container);
+  console.log(container);
+return container;
+}
+
+function createEntry(){
+
+
+  rowDiv.appendChild(colDiv);
+  return rowDiv;
+}
+
+function createRow(){
+  rowDiv = document.createElement("div");
+  rowDiv.className = "row";
+  return rowDiv;
+}
+
+function createCol(){
+  colDiv = document.createElement("div");
+  colDiv.className = "col-sm-4";
+  return colDiv;
+}
+
+function checkMainNodeLength(){
+  var mainNodes = $("#main")[0].childNodes;
+  var mainNodesLength = $("#main")[0].childNodes.length;
+  if (mainNodesLength > 1){
+    var latestNode = mainNodes[mainNodesLength-1];
+    if (checkRowNodeLength(latestNode)){
+      return latestNode;
+    }else {
+      return createRow();
+    }
+  }else {
+    return createRow();
+  }
+}
+
+function checkRowNodeLength(node){
+  if (node.childElementCount < 3){
+    return true;
+  }else if (node.childElementCount === 3){
+    return false;
+  }
+}
+
+function instaStuff(){
+  var outerContainer = checkMainNodeLength();
+// $("#main")[0].childNodes[1].childNodes.length
+  instaGramItems = filterResults("Instagram");
+  for(var i = 0; i < instaGramItems.length; i++){
+    var col = createCol();
+    var obj = instaGramItems[i]["item_data"];
+    imgTag = document.createElement("img");
+    pTag = document.createElement("p");
+    imgTag.setAttribute("src", obj["image"]["medium"]);
+    pTag.innerText = obj["caption"];
+    col.appendChild(imgTag);
+    col.appendChild(pTag);
+    outerContainer.appendChild(col);
+  }
+  $("#main").append(rowDiv);
 }
