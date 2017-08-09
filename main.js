a6c69025e317cb2fd8d0b235624fbd60c0842977
Peter Tascio <peter.tascio@gmail.com>
2017-08-09 15:43:44 -0400

    Remove access token

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function loadData(){
     url: "https://content.dropboxapi.com/1/files/auto/posts.json",
     type: 'GET',
     headers: {
-      "Authorization": "Bearer AkpoFy1WulgAAAAAAAAAfvVbPMzpOCMhfnekIX_OhQVk3yV6STBNLCinhWKXupnZ",
+      "Authorization": "Bearer ",
   },
   success: function (data) {
     content = JSON.parse(data);
